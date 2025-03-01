10d3459c3895104b80af3f8be0e01643f23561b6
mateushmd <mateushmdiniz@gmail.com>
2025-03-01 01:48:13 -0300

    :bug: bugfix(time): fixed a tiny mistake causing a major compilation error

--- core/time.cpp
+++ core/time.cpp
@@ -6,6 +6,9 @@ using namespace std::chrono;
 
 namespace emulator
 {
+    high_resolution_clock::time_point Time::start;
+    duration<double> Time::_deltaTime;
+
     void Time::captureStartTime()
     {
         start = high_resolution_clock::now();
@@ -14,7 +17,7 @@ namespace emulator
     void Time::calculateElapsedTime()
     {
         auto end = high_resolution_clock::now();
-        _deltaTime = duration_cast<duration<double>>(start - end);
+        _deltaTime = duration_cast<duration<double>>(end - start);
     }
 
     double Time::deltaTime()
