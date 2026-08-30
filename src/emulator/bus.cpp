a7b7f41557965e1d68691445b7e8365182ad9b69
mateushmd <mateushmdiniz@gmail.com>
2026-08-29 22:54:45 -0300

    fix(bus): using context now

--- src/emulator/bus.cpp
+++ src/emulator/bus.cpp
@@ -1,64 +1,117 @@
 #include "bus.h"
-#include "cartridge.hpp"
-#include "interrupts.h"
-#include "io/io_bus.h"
+#include "gb.h"
 
 namespace emulator {
-    Bus::Bus(Cartridge &cartridge, Interrupts &interrupts): 
-        cartridge(cartridge), io_bus(io::IOBus(interrupts)) { }
+    Bus::Bus() { }
 
-    uint8_t Bus::read(uint16_t address) {
+    uint8_t Bus::read(uint16_t address, struct EmulatorContext &context) {
         if (address < 0x4000) { // ROM bank 0
-            return cartridge.read_rom(address);
+            return context.cartridge.read_rom(address);
         } else if (address < 0x8000) { // ROM bank 1-n
             // CGB: implement bank switching
-            return cartridge.read_rom(address);
+            return context.cartridge.read_rom(address);
         } else if (address < 0xa000) { // vram
-            return vram[address - 0x8000];
+            return context.vram[address - 0x8000];
         } else if (address < 0xc000) { // eram
-            return cartridge.read_ram(address - 0xa000);
+            return context.cartridge.read_ram(address - 0xa000);
         } else if (address < 0xd000) { // wram bank 0
-            return wram[address - 0xc000];
+            return context.wram[address - 0xc000];
         } else if (address < 0xe000) { // wram bank 1-n
             // CGB: implement bank switching
-            return wram[address - 0xd000];
+            return context.wram[address - 0xd000];
         } else if (address < 0xfe00) {
             TODO("implement echo RAM");
         } else if (address < 0xfea0) {
-            return oam[address - 0xfe00];
+            return context.oam[address - 0xfe00];
         } else if (address < 0xff00) {
             TODO("implement not usable range");
+        } else if (address < 0xff01) {
+            //joypad
+        } else if (address < 0xff03) {
+            //serial transfer
+        } else if (address < 0xff04) {
+            TODO("unknown");
+        } else if (address < 0xff08) {
+            //timer and divider
+        } else if (address < 0xff0f) {
+            TODO("unknown");
+        } else if (address < 0xff10) {
+            //interrupts
+        } else if (address < 0xff27) {
+            //audio
+        } else if (address < 0xff30) {
+            TODO("unknown");
+        } else if (address < 0xff40) {
+            //wave pattern
+        } else if (address < 0xff46) {
+            //lcd 
+        } else if (address < 0xff47) {
+            //oam dma transfer
+        } else if (address < 0xff4c) {
+            //lcd
+        } else if (address < 0xff50) {
+            TODO("cgb");
+        } else if (address < 0xff51) {
+            //boot rom mapping control
         } else if (address < 0xff80) {
-            TODO("implement io");
+            TODO("cgb");
         } else if (address < 0xffff) {
-            return hram[address - 0xff80];
+            return context.hram[address - 0xff80];
         } else {
-            return ie;
+            return context.ie;
         }
     } 
 
-    void Bus::write(uint16_t address, uint8_t value) {
+    void Bus::write(uint16_t address, uint8_t value, struct EmulatorContext &context) {
         if ( address >= 0x8000 && address < 0xa000) { // vram
-            vram[address - 0x8000] = value;
+            context.vram[address - 0x8000] = value;
         } else if (address < 0xc000) { // eram
-            cartridge.write_ram(address - 0xa000, value);
+            context.cartridge.write_ram(address - 0xa000, value);
         } else if (address < 0xd000) { // wram bank 0
-            wram[address - 0xc000] = value;
+            context.wram[address - 0xc000] = value;
         } else if (address < 0xe000) { // wram bank 1-n
             // TODO: CGB: implement bank switching
-            wram[address - 0xd000] = value;
+            context.wram[address - 0xd000] = value;
         } else if (address < 0xfe00) {
             TODO("implement echo RAM");
         } else if (address < 0xfea0) {
-            oam[address - 0xfe00] = value;
+            context.oam[address - 0xfe00] = value;
         } else if (address < 0xff00) {
             TODO("implement not usable range");
+        } else if (address < 0xff01) {
+            //joypad
+        } else if (address < 0xff03) {
+            //serial transfer
+        } else if (address < 0xff04) {
+            TODO("unknown");
+        } else if (address < 0xff08) {
+            //timer and divider
+        } else if (address < 0xff0f) {
+            TODO("unknown");
+        } else if (address < 0xff10) {
+            //interrupts
+        } else if (address < 0xff27) {
+            //audio
+        } else if (address < 0xff30) {
+            TODO("unknown");
+        } else if (address < 0xff40) {
+            //wave pattern
+        } else if (address < 0xff46) {
+            //lcd 
+        } else if (address < 0xff47) {
+            //oam dma transfer
+        } else if (address < 0xff4c) {
+            //lcd
+        } else if (address < 0xff50) {
+            TODO("cgb");
+        } else if (address < 0xff51) {
+            //boot rom mapping control
         } else if (address < 0xff80) {
-            TODO("implement io");
+            TODO("cgb");
         } else if (address < 0xffff) {
-            hram[address - 0xff80] = value;
+            context.hram[address - 0xff80] = value;
         } else {
-            ie = value;
+            context.ie = value;
         }
     }
 }
--- src/emulator/bus.h
+++ src/emulator/bus.h
@@ -1,29 +1,15 @@
 #pragma once
 
-#include <array>
 #include <cstdint>
-#include "cartridge.hpp"
-#include "cartridge.hpp"
-#include "interrupts.h"
-#include "io/io_bus.h"
+
+#include "gb.h"
 
 namespace emulator {
     class Bus {
-        private:
-            std::array<uint8_t, 1024 * 8> vram;
-            std::array<uint8_t, 1024 * 8> wram;
-            std::array<uint8_t, 160> oam;
-            std::array<uint8_t, 127> hram;
-
-            Cartridge &cartridge;
-            io::IOBus io_bus;
-
-            uint8_t ie;
-
         public:
-            Bus(Cartridge &cartridge, Interrupts &interrupts);
+            Bus();
 
-            uint8_t read(uint16_t address);
-            void write(uint16_t address, uint8_t value);
+            uint8_t read(uint16_t address, struct EmulatorContext &context);
+            void write(uint16_t address, uint8_t value, struct EmulatorContext &context);
     };
 }
