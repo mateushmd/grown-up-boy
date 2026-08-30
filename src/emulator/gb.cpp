72e418503f31df86939681a350658c43e210079e
mateushmd <mateushmdiniz@gmail.com>
2026-08-29 22:54:22 -0300

    feat(gb): emulator context

--- src/emulator/gb.cpp
+++ src/emulator/gb.cpp
@@ -1,20 +1,42 @@
 #include "gb.h"
 
 #include <atomic>
+#include <array>
+#include <cstdint>
 
 #include "cpu.h"
-#include "interrupts.h"
 #include "io/audio.h"
-#include "io/io_bus.h"
 #include "io/joypad.h"
 #include "io/lcd.h"
 #include "io/timer.h"
 
 namespace emulator {
     Cartridge cartridge;
-    Interrupts interrputs;
-    Bus bus(cartridge, interrputs);
-    CPU cpu(bus);
+    std::array<uint8_t, 1024 * 8> vram;
+    std::array<uint8_t, 1024 * 8> wram;
+    std::array<uint8_t, 160> oam;
+    std::array<uint8_t, 127> hram;
+    io::Audio audio;
+    io::Joypad joypad;
+    io::LCD lcd;
+    io::Timer timer;
+    uint8_t ie;
+    
+    struct EmulatorContext context {
+        cartridge,
+        vram,
+        wram,
+        oam,
+        hram,
+        audio,
+        joypad,
+        lcd,
+        timer,
+        ie
+    };
+
+    Bus bus();
+    CPU cpu();
 
     std::atomic<bool> is_running; 
 
--- src/emulator/gb.h
+++ src/emulator/gb.h
@@ -1,6 +1,28 @@
 #pragma once
 
+#include <array>
+#include <cstdint>
+
+#include "cartridge.hpp"
+#include "io/audio.h"
+#include "io/joypad.h"
+#include "io/lcd.h"
+#include "io/timer.h"
+
 namespace emulator {
+    struct EmulatorContext {
+        Cartridge &cartridge;
+        std::array<uint8_t, 1024 * 8> &vram;
+        std::array<uint8_t, 1024 * 8> &wram;
+        std::array<uint8_t, 160> &oam;
+        std::array<uint8_t, 127> &hram;
+        io::Audio &audio;
+        io::Joypad &joypad;
+        io::LCD &lcd;
+        io::Timer &timer;
+        uint8_t &ie;
+    };
+
     void start();
     void stop();
 }
